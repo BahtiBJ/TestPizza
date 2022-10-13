976287ba15bc5fec93b69b5b8e33b5d1a01182b1
boyarstanov_b <andreyurk2016@gmail.com>
2022-10-13 22:59:42 +0300

    Create UI

---
+++ app/src/main/java/com/bbj/testpizza/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.bbj.testpizza
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainPager = findViewById<ViewPager2>(R.id.main_pager)
+        mainPager.isUserInputEnabled = false
+        mainPager.adapter = MainPagerAdapter(this)
+
+        val mainTabLayout = findViewById<TabLayout>(R.id.main_tab_layout)
+
+        TabLayoutMediator(mainTabLayout, mainPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = resources.getString(R.string.menu)
+                    tab.icon = ResourcesCompat.getDrawable(
+                        resources,
+                        R.drawable.ic_baseline_fastfood_24,
+                        theme
+                    )
+                }
+                else -> {}
+            }
+        }
+
+        // Choose city
+        val cityChoice = findViewById<LinearLayoutCompat>(R.id.toolbar_city_choice)
+
+        // Current city
+        val currentCity = findViewById<TextView>(R.id.toolbar_current_city)
+
+    }
+}
---
+++ app/src/main/java/com/bbj/testpizza/MainPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.bbj.testpizza
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position){
+            0 -> TODO()
+            else -> error("Unknown pager state")
+        }
+    }
+}
